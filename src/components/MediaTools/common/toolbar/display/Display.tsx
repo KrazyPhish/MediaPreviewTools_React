e5f91256122448dbfdbe743e535836ecee41ad4e
KrazyPhish <932781176@qq.com>
2023-09-27 15:25:51 +0800

    Display default props initialize changes

--- src/components/MediaTools/common/toolbar/display/Display.tsx
+++ src/components/MediaTools/common/toolbar/display/Display.tsx
@@ -5,12 +5,34 @@ import { DisplayConfig, DisplaySettings } from "../../../../../types/audio"
 import { debounce } from "../../utils"
 import { Form, Slider, Switch } from "antd"
 
-type DisplayProps = DeepRequired<DisplayConfig> & {
+type DisplayProps = {
+  options?: DisplayConfig
   onZoom: (mode: 'horizontal' | 'vertical', zoom: number) => void
   onSpect: (mode: 'spectrum' | 'spectrogram', spect: boolean) => void
 }
 
 const Display: React.FC<DisplayProps> = (props) => {
+  const options: DeepRequired<DisplayConfig> = {
+    spect: {
+      spectrum: props.options?.spect?.spectrum || true,
+      spectrogram: props.options?.spect?.spectrogram || false
+    },
+    zoomOptions: {
+      verticalZoom: {
+        init: props.options?.zoomOptions?.verticalZoom?.init ? props.options?.zoomOptions.verticalZoom.init : 1,
+        max: props.options?.zoomOptions?.verticalZoom?.max ? props.options?.zoomOptions.verticalZoom.max : 2,
+        min: props.options?.zoomOptions?.verticalZoom?.min ? props.options?.zoomOptions.verticalZoom.min : 1,
+        step: props.options?.zoomOptions?.verticalZoom?.step ? props.options?.zoomOptions.verticalZoom.step : 3
+      },
+      horizontalZoom: {
+        init: props.options?.zoomOptions?.horizontalZoom?.init ? props.options?.zoomOptions.horizontalZoom.init : 1,
+        max: props.options?.zoomOptions?.horizontalZoom?.max ? props.options?.zoomOptions.horizontalZoom.max : 2,
+        min: props.options?.zoomOptions?.horizontalZoom?.min ? props.options?.zoomOptions.horizontalZoom.min : 1,
+        step: props.options?.zoomOptions?.horizontalZoom?.step ? props.options?.zoomOptions.horizontalZoom.step : 0.01
+      }
+    }
+  }
+
   const spectHandler = debounce((mode: 'spectrogram' | 'spectrum', value: boolean) => props.onSpect(mode, value))
   const zoomHandler = debounce((mode: 'horizontal' | 'vertical', zoom: number) => props.onZoom(mode, zoom))
 
@@ -28,27 +50,35 @@ const Display: React.FC<DisplayProps> = (props) => {
     <div className="media-display">
       <Form layout="horizontal" size="small">
         <Form.Item label='频谱图'>
-          <Switch checked={props.spect.spectrum} disabled={!props.spect.spectrogram} onChange={(value) => onStateChange('spectrum', value)}/>
+          <Switch
+            checked={options.spect.spectrum}
+            disabled={!options.spect.spectrogram}
+            onChange={(value) => onStateChange('spectrum', value)}
+          />
         </Form.Item>
         <Form.Item label='语谱图'>
-          <Switch checked={props.spect.spectrogram} disabled={!props.spect.spectrum} onChange={(value) => onStateChange('spectrogram', value)}/>
+          <Switch
+            checked={options.spect.spectrogram}
+            disabled={!options.spect.spectrum}
+            onChange={(value) => onStateChange('spectrogram', value)}
+          />
         </Form.Item>
         <Form.Item label='水平缩放'>
           <Slider
-            value={props.zoomOptions.horizontalZoom.init}
-            min={props.zoomOptions.horizontalZoom.min}
-            max={props.zoomOptions.horizontalZoom.max}
-            step={props.zoomOptions.horizontalZoom.step}
+            value={options.zoomOptions.horizontalZoom.init}
+            min={options.zoomOptions.horizontalZoom.min}
+            max={options.zoomOptions.horizontalZoom.max}
+            step={options.zoomOptions.horizontalZoom.step}
             tooltip={{ open: false }}
             onChange={(value) => onStateChange('horizontalZoom', value)}
           />
         </Form.Item>
         <Form.Item label='垂直缩放'>
           <Slider
-            value={props.zoomOptions.verticalZoom.init}
-            min={props.zoomOptions.verticalZoom.min}
-            max={props.zoomOptions.verticalZoom.max}
-            step={props.zoomOptions.verticalZoom.step}
+            value={options.zoomOptions.verticalZoom.init}
+            min={options.zoomOptions.verticalZoom.min}
+            max={options.zoomOptions.verticalZoom.max}
+            step={options.zoomOptions.verticalZoom.step}
             tooltip={{ open: false }}
             onChange={(value) => onStateChange('verticalZoom', value)}
           />
